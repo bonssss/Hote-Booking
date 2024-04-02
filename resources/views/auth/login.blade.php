fa686c1d6f545201c68985c9d5f6e4dd1670f575
bonsa <bons6710hos@gmail.com>
2024-04-03 00:41:19 +0300

    register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -109,13 +109,13 @@
 							<div class="row">
 								<div class="col-md-12">
 									<div class="form-group">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+			    					    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-@error('email')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-@enderror
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
 			    				    </div>
 								</div>
                                
